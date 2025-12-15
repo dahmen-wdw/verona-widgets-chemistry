c9130c2bcd017b8d154d159261a1e7b960284df9
irathgen <rathgent@iqb.hu-berlin.de>
2025-12-15 12:22:07 +0100

    add simple home page to showcase app, update meta data

---
+++ projects/showcase-widgets/src/home-page/home-page.ts
@@ -0,0 +1,9 @@
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.html',
+  styleUrls: ['./home-page.scss'],
+})
+export class HomePage {
+}
--- projects/showcase-widgets/src/showcase-app/showcase-app.routes.ts
+++ projects/showcase-widgets/src/showcase-app/showcase-app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { PsSelectPage } from '../ps-select-page/ps-select-page';
 import { MoleculeEditorPage } from '../molecule-editor-page/molecule-editor-page';
+import { HomePage } from '../home-page/home-page';
 
 export const enum ShowcasePath {
   PeriodicSystemSelect = 'ps-select',
@@ -8,6 +9,10 @@ export const enum ShowcasePath {
 }
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+  },
   {
     path: ShowcasePath.PeriodicSystemSelect,
     component: PsSelectPage,
